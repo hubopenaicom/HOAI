bf7b23aebe38d950c1d65a8aa70a3d5fe775e4f2
hubopenaicom <admin@hubopenai.com>
2026-05-05 23:24:58 -0700

    Add new JavaScript files for various functionalities: created account log, Aliyun OSS settings, auto-reply management, bad words filtering, and Baidu statistics modules. Removed obsolete compressed files to streamline asset management.

--- AIWebQuickDeploy/public/admin/assets/models-BCMoUStS.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * https://fantastic-admin.hurui.me
- */
-  
-import{i as e}from"./auth-a3Sv0hX5.js";var t={queryModels:t=>e.get(`models/query`,{params:t}),setModels:t=>e.post(`models/setModel`,t),delModels:t=>e.post(`models/delModel`,t)};export{t};
