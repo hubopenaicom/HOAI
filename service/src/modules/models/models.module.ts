0c34a43ca89824cd4744dbe25653e378cb921a8c
hubopenaicom <admin@hubopenai.com>
2026-05-06 05:08:34 -0700

    Update ModelsModule to include ModelTokenCatalogEntity and ModelTokenCatalogService: enhanced module imports and providers to support token catalog management functionalities.

--- service/src/modules/models/models.module.ts
+++ service/src/modules/models/models.module.ts
@@ -3,13 +3,15 @@ import { ModelsController } from './models.controller';
 import { ModelsService } from './models.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModelsEntity } from './models.entity';
+import { ModelTokenCatalogEntity } from './model-token-catalog.entity';
+import { ModelTokenCatalogService } from './model-token-catalog.service';
 // import { ModelsTypeEntity } from './modelType.entity';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([ModelsEntity])],
+  imports: [TypeOrmModule.forFeature([ModelsEntity, ModelTokenCatalogEntity])],
   controllers: [ModelsController],
-  providers: [ModelsService],
-  exports: [ModelsService],
+  providers: [ModelsService, ModelTokenCatalogService],
+  exports: [ModelsService, ModelTokenCatalogService],
 })
 export class ModelsModule {}
