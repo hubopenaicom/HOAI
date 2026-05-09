a28a57836ff44ecaefb797a75a80aa5ad7f91f33
hubopenaicom <admin@hubopenai.com>
2026-05-08 22:58:15 -0700

    Update admin assets and HTML: replace outdated script and stylesheet links, add new account log and Aliyun configuration files, and remove deprecated files to streamline asset management and enhance application performance.

--- AIWebQuickDeploy/public/admin/assets/config-HYviJwqR.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * https://fantastic-admin.hurui.me
- */
-  
-import{i as e}from"./auth-16_8zYL8.js";var t={queryAllConfig:()=>e.get(`config/queryAll`),queryConfig:t=>e.post(`config/query`,t),setConfig:t=>e.post(`config/set`,t)};export{t};
--- AIWebQuickDeploy/public/admin/assets/models-BOklq8mm.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * https://fantastic-admin.hurui.me
- */
-  
-import{i as e}from"./auth-16_8zYL8.js";var t={queryModels:t=>e.get(`models/query`,{params:t}),setModels:t=>e.post(`models/setModel`,t),delModels:t=>e.post(`models/delModel`,t),queryTokenCatalog:t=>e.get(`models/queryTokenCatalog`,{params:t}),setTokenCatalog:t=>e.post(`models/setTokenCatalog`,t),delTokenCatalog:t=>e.post(`models/delTokenCatalog`,t),syncTokenCatalog:t=>e.post(`models/syncTokenCatalog`,t),syncTokenCatalogProgress:t=>e.get(`models/syncTokenCatalogProgress`,{params:t}),lookupTokenCatalog:t=>e.get(`models/lookupTokenCatalog`,{params:t})};export{t};
