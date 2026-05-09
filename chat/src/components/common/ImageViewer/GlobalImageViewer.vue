40bdc1f03b28e184784b6ac7d2af48c96786f580
hubopenaicom <admin@hubopenai.com>
2026-05-08 22:32:01 -0700

    Add captions to GlobalImageViewer: include original and translated captions in the image viewer component for enhanced user context.

--- chat/src/components/common/ImageViewer/GlobalImageViewer.vue
+++ chat/src/components/common/ImageViewer/GlobalImageViewer.vue
@@ -3,6 +3,8 @@
     v-model:visible="isVisible"
     :image-url="currentImageUrl"
     :file-name="currentFileName"
+    :caption-original="currentCaptionOriginal"
+    :caption-translated="currentCaptionTranslated"
     @close="handleClose"
   />
 </template>
@@ -11,7 +13,14 @@
 import ImageViewer from './index.vue'
 import { useImageViewer } from './useImageViewer'
 
-const { isVisible, currentImageUrl, currentFileName, closeImageViewer } = useImageViewer()
+const {
+  isVisible,
+  currentImageUrl,
+  currentFileName,
+  currentCaptionOriginal,
+  currentCaptionTranslated,
+  closeImageViewer,
+} = useImageViewer()
 
 function handleClose() {
   closeImageViewer()
