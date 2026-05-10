b1d3515be9059b4f7816a862e55de39269a0efab
hubopenaicom <admin@hubopenai.com>
2026-05-09 20:37:34 -0700

    Enhance DrawingMjModule by adding UserEntity and DrawingMjAdminController: update imports to include UserEntity for user-related functionalities and add DrawingMjAdminController to support admin operations, improving overall module capabilities.

--- service/src/modules/drawingMj/drawing-mj.module.ts
+++ service/src/modules/drawingMj/drawing-mj.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModelsEntity } from '../models/models.entity';
+import { UserEntity } from '../user/user.entity';
+import { DrawingMjAdminController } from './drawing-mj-admin.controller';
 import { DrawingMjController } from './drawing-mj.controller';
 import { DrawingMjJobEntity } from './drawing-mj-job.entity';
 import { DrawingMjJobService } from './drawing-mj-job.service';
 import { DrawingMjService } from './drawing-mj.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ModelsEntity, DrawingMjJobEntity])],
-  controllers: [DrawingMjController],
+  imports: [TypeOrmModule.forFeature([ModelsEntity, DrawingMjJobEntity, UserEntity])],
+  controllers: [DrawingMjController, DrawingMjAdminController],
   providers: [DrawingMjService, DrawingMjJobService],
   exports: [DrawingMjService],
 })
