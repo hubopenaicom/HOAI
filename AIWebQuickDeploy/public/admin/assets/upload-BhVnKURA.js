cb9b1dde6f4a847382fe8d45fc1a3c93c9874025
hubopenaicom <admin@hubopenai.com>
2026-05-14 20:13:15 -0700

    Update admin assets and components: replace outdated files, add new account log and auto-reply features, enhance Baidu integration with new settings, and streamline project structure by removing obsolete files.

---
+++ AIWebQuickDeploy/public/admin/assets/upload-BhVnKURA.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+  
+import{i as e}from"./auth-BhR30MXU.js";var t={uploadFile:(t,n)=>{let r=n?`upload/file?dir=${encodeURIComponent(n)}`:`upload/file`;return e.post(r,t)}};export{t};
